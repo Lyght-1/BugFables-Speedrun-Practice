2b790b36f3dc37c027670dc8f78f31f8670d5dfb
Alexandre Cabana <58051353+Lyght-1@users.noreply.github.com>
2023-04-23 16:55:14 -0400

    Update SpeedrunPracticePlugin.cs

--- SpeedrunPracticePlugin.cs
+++ SpeedrunPracticePlugin.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 
 namespace SpeedrunPractice
 {
-    [BepInPlugin("com.Lyght.BugFables.plugins.SpeedrunPractice", "Speedrun Practice", "5.0.2")]
+    [BepInPlugin("com.Lyght.BugFables.plugins.SpeedrunPractice", "Speedrun Practice", "5.0.3")]
     [BepInProcess("Bug Fables.exe")]
     public class SpeedrunPracticePlugin : BaseUnityPlugin
     {
